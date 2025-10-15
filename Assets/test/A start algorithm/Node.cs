ff1abaeb9739707b247b39f094d3921045863e81
ducan8 <ducan9c@gmail.com>
2025-10-15 23:22:42 +0700

    update grid for a* algorithm

--- Assets/test/A start algorithm/Node.cs
+++ Assets/test/A start algorithm/Node.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node : MonoBehaviour
+public class Node
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 worldPosition;
+    public bool walkable;
 
-    // Update is called once per frame
-    void Update()
+    public Node(Vector3 _worldPos, bool _walkable)
     {
-        
+        worldPosition = _worldPos;
+        walkable = _walkable;
     }
 }
